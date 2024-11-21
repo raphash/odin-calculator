9a0947c41108747c91a7975ee986c7b5854b68ec
raphash <175216103+raphash@users.noreply.github.com>
2024-11-21 03:13:00 -0300

    feat: Fix multiple dot adding bug
    This fixes a bug that can insert a lot of dots in
    the number, whit this fix now is possible insert
    only one dot in the number.

--- script.js
+++ script.js
@@ -11,32 +11,46 @@ const sub = (a, b) => a - b;
 const mul = (a, b) => a * b;
 const div = (a, b) => a / b;
 
+// This rounds the operand with decimal points to a precise number without unwanted results.
 function operate(operand1, operator, operand2) {
   switch (operator) {
-    case "+": return Math.round(sum(parseFloat(operand1), parseFloat(operand2)) * 10) / 10;
-    case "-": return Math.round(sub(parseFloat(operand1), parseFloat(operand2)) * 10) / 10;
-    case "÷": return Math.round(div(parseFloat(operand1), parseFloat(operand2)) * 10) / 10;
-    case "×": return Math.round(mul(parseFloat(operand1), parseFloat(operand2)) * 10) / 10;
+    case "+": return Math.round(sum(parseFloat(operand1), parseFloat(operand2)) * 1000000000000000) / 1000000000000000;
+    case "-": return Math.round(sub(parseFloat(operand1), parseFloat(operand2)) * 1000000000000000) / 1000000000000000;
+    case "÷": return Math.round(div(parseFloat(operand1), parseFloat(operand2)) * 1000000000000000) / 1000000000000000;
+    case "×": return Math.round(mul(parseFloat(operand1), parseFloat(operand2)) * 1000000000000000) / 1000000000000000;
   }
 }
 
 // Logic of numeric buttons
 numbers.forEach((number) => {
-  number.addEventListener("click", ()=>{
-    
-    if (!operation[1]) {
-      operation[0] += number.textContent;
-      display.textContent = operation[0];
-    } 
+  if (number.classList.contains("dot")) {
+    number.addEventListener("click", ()=>{
+      if (!display.textContent.includes(".")) {
+        display.textContent += ".";
 
-    else if (operation[1]) {
-      operation[2] += number.textContent;
-      display.textContent = operation[2];
-    }
-
-    console.clear();
-    console.log(operation)
-  });
+        // Logic to check what is the current number to add the dot.
+        if (!operation[2]) {
+          operation[0] = display.textContent;
+        } 
+        else if (operation[1]) {
+          operation[2] = display.textContent;
+        }
+      }
+    });
+  }
+  else {
+    number.addEventListener("click", ()=>{
+      if (!operation[1]) {
+        operation[0] += number.textContent;
+  
+        display.textContent = operation[0];
+      } 
+      else if (operation[1]) {
+        operation[2] += number.textContent;
+        display.textContent = operation[2];
+      }
+    });
+  }
 });
 
 // Logic of arithmetic operators
@@ -44,16 +58,14 @@ operators.forEach((operator) => {
   operator.addEventListener("click", ()=>{
     if (operator.textContent != "=") {
       operation[1] = operator.textContent;
-    } else if (operation[1] && operation[2]) {
+    } 
+    else if (operation[1] && operation[2]) {
       display.textContent = operate(... operation);
       
       operation[0] = display.textContent;
       operation[1] = "";
       operation[2] = "";
     }
-
-    console.clear();
-    console.log(operation)
   });
 });
 
@@ -63,6 +75,5 @@ clear.addEventListener("click", ()=>{
   operation[1] = "";
   operation[2] = "";
 
-  console.clear();
   display.textContent = 0;
 });
